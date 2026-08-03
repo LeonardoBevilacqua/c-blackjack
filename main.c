4f79af391eb5b3372de51106aadf285ad2f5b583
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2026-08-03 10:31:32 -0300

    moving frequently used variable to global scope

--- main.c
+++ main.c
@@ -22,7 +22,16 @@ typedef struct {
     size_t npc;
 } PLAYER;
 
-size_t already_used(CARD card, CARD used_cards[DECK_SIZE])
+const CARD cards[] = {
+    { 'C', "2", 2, FALSE }, { 'C', "3", 3, FALSE }, { 'C', "4", 4, FALSE }, { 'C', "5", 5, FALSE }, { 'C', "6", 6, FALSE }, { 'C', "7", 7, FALSE }, { 'C', "8", 8, FALSE }, { 'C', "9", 9, FALSE }, { 'C', "10", 10, FALSE }, { 'C', "J", 10, FALSE }, { 'C', "Q", 10, FALSE }, { 'C', "K", 10, FALSE }, { 'C', "A", 1, TRUE },
+    { 'D', "2", 2, FALSE }, { 'D', "3", 3, FALSE }, { 'D', "4", 4, FALSE }, { 'D', "5", 5, FALSE }, { 'D', "6", 6, FALSE }, { 'D', "7", 7, FALSE }, { 'D', "8", 8, FALSE }, { 'D', "9", 9, FALSE }, { 'D', "10", 10, FALSE }, { 'D', "J", 10, FALSE }, { 'D', "Q", 10, FALSE }, { 'D', "K", 10, FALSE }, { 'D', "A", 1, TRUE },
+    { 'H', "2", 2, FALSE }, { 'H', "3", 3, FALSE }, { 'H', "4", 4, FALSE }, { 'H', "5", 5, FALSE }, { 'H', "6", 6, FALSE }, { 'H', "7", 7, FALSE }, { 'H', "8", 8, FALSE }, { 'H', "9", 9, FALSE }, { 'H', "10", 10, FALSE }, { 'H', "J", 10, FALSE }, { 'H', "Q", 10, FALSE }, { 'H', "K", 10, FALSE }, { 'H', "A", 1, TRUE },
+    { 'S', "2", 2, FALSE }, { 'S', "3", 3, FALSE }, { 'S', "4", 4, FALSE }, { 'S', "5", 5, FALSE }, { 'S', "6", 6, FALSE }, { 'S', "7", 7, FALSE }, { 'S', "8", 8, FALSE }, { 'S', "9", 9, FALSE }, { 'S', "10", 10, FALSE }, { 'S', "J", 10, FALSE }, { 'S', "Q", 10, FALSE }, { 'S', "K", 10, FALSE }, { 'S', "A", 1, TRUE },
+};
+CARD used_cards[DECK_SIZE] = {};
+short used_cards_index = 0;
+
+size_t already_used(CARD card)
 {
     for (short i = 0; i < DECK_SIZE; ++i) {
         if (!used_cards[i].label && !used_cards[i].value) break;
@@ -31,25 +40,22 @@ size_t already_used(CARD card, CARD used_cards[DECK_SIZE])
     return FALSE;
 }
 
-// TODO transform cards in global
-CARD get_card(const CARD cards[], CARD used_cards[DECK_SIZE])
+CARD get_card()
 {
     CARD card;
 
     do card = cards[rand() % DECK_SIZE]; 
-    while (already_used(card, used_cards));
+    while (already_used(card));
 
     return card;
 }
 
-void handle_player_cards(
-    short* used_cards_index,
-    PLAYER* current_player,
-    const CARD cards[],
-    CARD used_cards[])
+void handle_player_cards(PLAYER* current_player)
 {
-    while (*used_cards_index++ < DECK_SIZE && current_player->score < BLACKJACK) {
-        CARD card = get_card(cards, used_cards);
+    while (used_cards_index < DECK_SIZE && current_player->score < BLACKJACK) {
+        CARD card = get_card();
+        used_cards[++used_cards_index] = card;
+
         current_player->score += card.value;
         printf("%c%s - Score %d\n", card.suit, card.label, current_player->score);
 
@@ -69,14 +75,6 @@ int main()
 {
     srand(time(NULL));
 
-    const CARD cards_2[] = {
-        { 'C', "2", 2, FALSE }, { 'C', "3", 3, FALSE }, { 'C', "4", 4, FALSE }, { 'C', "5", 5, FALSE }, { 'C', "6", 6, FALSE }, { 'C', "7", 7, FALSE }, { 'C', "8", 8, FALSE }, { 'C', "9", 9, FALSE }, { 'C', "10", 10, FALSE }, { 'C', "J", 10, FALSE }, { 'C', "Q", 10, FALSE }, { 'C', "K", 10, FALSE }, { 'C', "A", 1, TRUE }, 
-        { 'D', "2", 2, FALSE }, { 'D', "3", 3, FALSE }, { 'D', "4", 4, FALSE }, { 'D', "5", 5, FALSE }, { 'D', "6", 6, FALSE }, { 'D', "7", 7, FALSE }, { 'D', "8", 8, FALSE }, { 'D', "9", 9, FALSE }, { 'D', "10", 10, FALSE }, { 'D', "J", 10, FALSE }, { 'D', "Q", 10, FALSE }, { 'D', "K", 10, FALSE }, { 'D', "A", 1, TRUE }, 
-        { 'H', "2", 2, FALSE }, { 'H', "3", 3, FALSE }, { 'H', "4", 4, FALSE }, { 'H', "5", 5, FALSE }, { 'H', "6", 6, FALSE }, { 'H', "7", 7, FALSE }, { 'H', "8", 8, FALSE }, { 'H', "9", 9, FALSE }, { 'H', "10", 10, FALSE }, { 'H', "J", 10, FALSE }, { 'H', "Q", 10, FALSE }, { 'H', "K", 10, FALSE }, { 'H', "A", 1, TRUE }, 
-        { 'S', "2", 2, FALSE }, { 'S', "3", 3, FALSE }, { 'S', "4", 4, FALSE }, { 'S', "5", 5, FALSE }, { 'S', "6", 6, FALSE }, { 'S', "7", 7, FALSE }, { 'S', "8", 8, FALSE }, { 'S', "9", 9, FALSE }, { 'S', "10", 10, FALSE }, { 'S', "J", 10, FALSE }, { 'S', "Q", 10, FALSE }, { 'S', "K", 10, FALSE }, { 'S', "A", 1, TRUE }, 
-    };
-    CARD used_cards[DECK_SIZE] = {};
-    short used_cards_index = 0;
     PLAYER players[] = { { "Player", {}, 0, FALSE }, { "House", {}, 0, TRUE } };
     PLAYER* winner = NULL;
 
@@ -84,7 +82,7 @@ int main()
         PLAYER* current_player = &players[i];
         printf("Player: %s\n", current_player->name);
 
-        handle_player_cards(&used_cards_index, current_player, cards_2, used_cards);
+        handle_player_cards(current_player);
 
         printf("Final score: %d\n\n", current_player->score);
         if (current_player->score <= BLACKJACK) winner = current_player;
